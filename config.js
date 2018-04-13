f741c415a693e4ba98b511720637937f93d9a5a8
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-04-12 22:45:13 -0300

    gitignore

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 export const configBot = {
-  token: 'NDM0MDQ0NzgyMzgyNDE1ODg1.DbEtpg.__c6cedxIdWxoszwDIH4z-z0EWc',
-  private: true
+  token: 'NDM0MDQ0NzgyMzgyNDE1ODg1.DbEtpg.__c6cedxIdWxoszwDIH4z-z0EWc'
 }
