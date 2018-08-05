6136aa8b6cbfe8fbdf24f2dd51a3b4fb1d70096d
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-08-05 19:19:20 -0400

    add newspapers

--- libs/kiosko.js
+++ libs/kiosko.js
@@ -10,6 +10,24 @@ export async function kiosko(news) {
     case 'lacuarta':
       url = `http://img.kiosko.net/${date}/cl/cl_cuarta.750.jpg`
       break
+    case 'times':
+      url = `http://img.kiosko.net/${date}/uk/the_times.750.jpg`
+      break
+    case 'elpais':
+      url = `http://img.kiosko.net/${date}/es/elpais.750.jpg`
+      break
+    case 'nytimes':
+      url = `http://img.kiosko.net/${date}/us/newyork_times.750.jpg`
+      break
+    case 'mercurio':
+      url = `http://img.kiosko.net/${date}/cl/cl_mercurio.750.jpg`
+      break
+    case 'clarin':
+      url = `http://img.kiosko.net/${date}/ar/ar_clarin.750.jpg`
+      break
+    default:
+      url = `No se encuentra disponible 📄`
+      break
   }
   return url
 }
