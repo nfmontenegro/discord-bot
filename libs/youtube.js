13a70e9b1fb2acc733e57d7b89a50db8ddd53605
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-08-06 21:25:20 -0400

    get random video from youtube

--- libs/youtube.js
+++ libs/youtube.js
@@ -2,15 +2,14 @@ import Youtube from 'youtube-node'
 
 function requestYoutube(params) {
   return new Promise((resolve, reject) => {
-    const youtube = new Youtube();
+    const youtube = new Youtube()
     youtube.setKey(process.env.YOUTUBE)
 
-    youtube.search(params, 1, (error, data) => {
+    youtube.search(params, 30, (error, data) => {
       if (error) {
         reject('No se pudo encontrar el video 📼')
       } else {
-        const response = `https://www.youtube.com/watch?v=${data.items[0].id.videoId}`
-        resolve(response)
+        resolve(data)
       }
     })
   })
@@ -18,10 +17,12 @@ function requestYoutube(params) {
 
 export async function youtube(params) {
   try {
-    const search = await requestYoutube(params)
-    return search
+    const { items } = await requestYoutube(params)
+    const videosId = items.map(item => item.id.videoId).filter(data => data)
+
+    const randomVideo = videosId[Math.floor(Math.random() * videosId.length)]
+    return `https://www.youtube.com/watch?v=${randomVideo}`
   } catch (err) {
-    console.log('Youtube Error:', err)
+    return err
   }
 }
-
