167858eaf4cecdb5124a52faa087d939210ea496
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-11-30 12:26:17 -0300

    remove comments

--- libs/weather.js
+++ libs/weather.js
@@ -11,14 +11,11 @@ export async function weather(city) {
 
   const response = await fetch(urlCity)
   const {location, day} = await response.json()
+
   const nextWeather = Object.values(day).map(
     ({name, tempmin, tempmax, symbol_description}) =>
       `Para el día ${name} ${symbol_description}, la mínima sera ${tempmin}º y la máxima ${tempmax}º \n`
   )
-  // const nextWeather = Objectday.map(
-  //   ({name, tempmin, tempmax, symbol_description}) =>
-  //     `Para el día ${name} ${symbol_description}, la mínima sera ${tempmin}º y la máxima ${tempmax}º \n`
-  // )
 
   return `El clima 🌤 ☁️ 🌞 para ${location} \n\n ${nextWeather.join('\n')}`
 }
