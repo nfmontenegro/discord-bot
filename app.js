909c55cae4b331ebf86ef14c7459b0c05652b607
nfmontenegro <nicolas.fmontenegro@gmail.com>
2019-01-16 17:04:42 -0300

    httpserver

--- app.js
+++ app.js
@@ -1,86 +1,84 @@
-import express from 'express'
+import http from 'http'
 import Discord from 'discord.io'
+
 import utils from './utils'
 
 require('dotenv').config()
 
-const app = express()
-
-const bot = new Discord.Client({
-  token: process.env.TOKEN,
-  autorun: true
-})
-
-bot.on('ready', () => {
-  console.log('=> Init Bot ....')
-  console.log('=> Ready!  ✨')
-  console.log('___________________')
-  console.log('\n')
-  console.log(`=> ${bot.username.toUpperCase()} Connected! 🤖`)
-})
-
-bot.on('message', (user, userID, channelID, message, event) => {
-  const {
-    type,
-    author: {id}
-  } = event.d
-
-  if (type === 7 && id !== process.env.DISCORDIDBOT) {
-    bot.sendMessage({
-      to: channelID,
-      message: `Hola 👋! ${user}, Bienvenido a la comunidad de Javascript en Español 🍻`
-    })
-  }
-
-  utils.commands.map(command => {
-    if (message.includes(command)) {
-      const argument = utils.cleanCommand(command, message)
-      if (
-        id === process.env.DISCORDIDADMIN ||
-        id === process.env.DISCORDIDBOT
-      ) {
-        console.log('=> type by:', event.d.author)
-        console.log('\n')
-        console.log('=> user id:', userID)
-        console.log('\n')
-
-        const lib = utils.requestLib(command)
-
-        lib(argument)
-          .then(message => {
-            bot.sendMessage({
-              to: channelID,
-              message
-            })
-          })
-          .catch(error => console.log('Err:', error))
-      } else {
-        console.log('=> Message', message)
-        console.log('=> Dont have permission!: ', event.d.author)
-      }
-    }
+const app = http.createServer((request, response) => {
+  const bot = new Discord.Client({
+    token: process.env.TOKEN,
+    autorun: true
   })
 
-  if (message === '!createinvite') {
-    bot.createInvite({channelID}, (err, {code}) => {
-      if (!err) {
-        bot.sendMessage({
-          to: channelID,
-          message: `https://discord.gg/${code}`
-        })
+  bot.on('ready', () => {
+    console.log('=> Init Bot ....')
+    console.log('=> Ready!  ✨')
+    console.log('___________________')
+    console.log('\n')
+    console.log(`=> ${bot.username.toUpperCase()} Connected! 🤖`)
+  })
+
+  bot.on('message', (user, userID, channelID, message, event) => {
+    const {
+      type,
+      author: {id}
+    } = event.d
+
+    if (type === 7 && id !== process.env.DISCORDIDBOT) {
+      bot.sendMessage({
+        to: channelID,
+        message: `Hola 👋! ${user}, Bienvenido a la comunidad de Javascript en Español 🍻`
+      })
+    }
+
+    utils.commands.map(command => {
+      if (message.includes(command)) {
+        const argument = utils.cleanCommand(command, message)
+        if (
+          id === process.env.DISCORDIDADMIN ||
+          id === process.env.DISCORDIDBOT
+        ) {
+          console.log('=> type by:', event.d.author)
+          console.log('\n')
+          console.log('=> user id:', userID)
+          console.log('\n')
+
+          const lib = utils.requestLib(command)
+
+          lib(argument)
+            .then(message => {
+              bot.sendMessage({
+                to: channelID,
+                message
+              })
+            })
+            .catch(error => console.log('Err:', error))
+        } else {
+          console.log('=> Message', message)
+          console.log('=> Dont have permission!: ', event.d.author)
+        }
       }
     })
-  }
 
-  if (message === '!help' || message === '!ayuda') {
-    bot.sendMessage({
-      to: channelID,
-      message: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-    })
-  }
+    if (message === '!createinvite') {
+      bot.createInvite({channelID}, (err, {code}) => {
+        if (!err) {
+          bot.sendMessage({
+            to: channelID,
+            message: `https://discord.gg/${code}`
+          })
+        }
+      })
+    }
+
+    if (message === '!help' || message === '!ayuda') {
+      bot.sendMessage({
+        to: channelID,
+        message: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+      })
+    }
+  })
 })
 
-const port = process.env.PORT || 8080
-app.listen(port, () => {
-  console.log('Express server listening on port', port)
-})
+app.listen(3000, () => console.log('Server listen port: 3000'))
